3be49f1aa8ff858d0930a91621d78a1435a0a456
malo chaperon <chaperon.malo@gmail.com>
2020-10-24 18:06:33 +0200

    init

---
+++ public/js/dist/composant.js
@@ -0,0 +1,237 @@
+// scroll reveal anime
+const sr = ScrollReveal();
+
+// <!-- Hero header image -->
+sr.reveal('.left-hero-title', {
+	 delay: 0,
+    duration: 2000,
+    distance: "80px",
+    origin: "bottom"
+});
+
+sr.reveal('.right-hero-title', {
+	 delay: 0,
+    duration: 2000,
+    distance: "80px",
+    origin: "bottom"
+});
+
+// <!-- blockquote texte simple fond noir -->
+sr.reveal('.h2-white', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h3-white', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.cta-blockquote-white', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.h2-black', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h3-black', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.cta-blockquote-black', {
+	 delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// <!-- Texte intro big -->
+sr.reveal('.big-text-intro', {
+	 delay: 1,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.min-text-intro', {
+	 delay: 2,
+    duration: 3000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// <!-- Blockquote big text -->
+sr.reveal('.blockquote-big-texte-white', {
+	 delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.blockquote-big-texte-black', {
+	 delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// <!-- Texte form -->
+sr.reveal('.text-content', {
+	 delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// grid photo
+sr.reveal('.box-min', {
+	 delay: 2,
+    duration: 3000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.box-middle', {
+	 delay: 2,
+    duration: 3000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.box-full', {
+	 delay: 2,
+    duration: 3000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// grid photo
+sr.reveal('.grid-photo-carre', {
+	 delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// video-player
+sr.reveal('.video-player', {
+	 delay: 2,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+// banner image
+sr.reveal('.content-banner-image-clm', {
+	 delay: 1,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+// grid text black
+sr.reveal('.box-grid-text', {
+	 delay: 1,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+// footer
+sr.reveal('.text-footer', {
+	 delay: 1,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+
+sr.reveal('.h5-black', {
+     delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('#btn-classic', {
+     delay: 100,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.bottom-project-section', {
+     delay: 0,
+    duration: 1000,
+    distance: "80px",
+    origin: "bottom"
+});
+
+sr.reveal('.top-image-section', {
+     delay: 0,
+    duration: 2000,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.title-home-intro', {
+     delay: 0,
+    duration: 2000,
+    distance: "90px",
+    origin: "bottom"
+});
+
+sr.reveal('.home-image-section', {
+     delay: 0,
+    duration: 2000,
+    distance: "90px",
+    origin: "bottom"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
